8bafb76c7ab45423cb8697d3065a0fa6be5c9753
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-26 23:22:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-//Middlewares
-app.use(express.json());
 
-// Write a GET Request to get the next number from the input 'num'.
-// Endpoint : /api/get-next-num
-// Return the response as {message : , status: }
+//Router Middlewares
+app.use(express.json());
+app.get('/api/get-next-num', (req, res) => {
+    let { num } = req.body;
+    res.send({ message: num + 1, status: 'success' });
+});
 
 module.exports = app;
